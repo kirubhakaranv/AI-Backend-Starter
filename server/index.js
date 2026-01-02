bea6cafb8ddeeb39b768c5ce741adcf47bf952b3
kirubha <kirubhakaran.v2023eee@sece.ac.in>
2026-01-02 19:15:30 +0530

    feat: implement /chat API endpoint with AI response

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express=require('express');
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const dotenv=require('dotenv');
+const OpenAI = require("openai");
 
 dotenv.config();
-import OpenAI from "openai";
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
@@ -22,6 +23,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const port=process.env.port || 3000;
+const charroutes=require('./routes/chatRoutes');
+app.use("/",charroutes)
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
---
+++ server/routes/chatRoutes.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const dotenv=require('dotenv');
+const OpenAI = require("openai");
+const router=express.Router();
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+
+
+router.post("/chat",async(req,res)=>{
+    try{
+      const {message}=req.body;
+        const response = await openai.responses.create({
+  model: "gpt-5-nano",
+  input: message,
+  store: true,
+});
+res.json({
+    reply:response.output_text,
+});
+
+    }catch(error){
+        res.status(500).send(error.message);
+        }
+})
+
+module.exports = router;
