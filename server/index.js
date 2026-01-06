160ac17b43595775c930358208788116135ec58e
kirubha <kirubhakaran.v2023eee@sece.ac.in>
2026-01-06 14:40:01 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -5,27 +5,30 @@ const dotenv=require('dotenv');
 const OpenAI = require("openai");
 const connectDB=require('./db/connectDB');
 
+
 dotenv.config();
 
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY
+// });
 
-const response = openai.responses.create({
-  model: "gpt-5-nano",
-  input: "write a haiku about ai",
-  store: true,
-});
+// const response = openai.responses.create({
+//   model: "gpt-5-nano",
+//   input: "write a haiku about ai",
+//   store: true,
+// });
 
-response.then((result) => console.log(result.output_text));
+// response.then((result) => console.log(result.output_text));
 const app=express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const port=process.env.port || 3000;
-const charroutes=require('./routes/chatRoutes');
-app.use("/",charroutes)
+//const charroutes=require('./routes/chatRoutes');
+const authRoutes=require('./routes/authRoutes');
+// app.use("/",charroutes)
+app.use("/auth",authRoutes)
 app.listen(port,()=>{
     connectDB();
     console.log(`Server is running on port ${port}`);
